6351e60db27abb91321daacc4e375ae3f7d339a4
Muhammad Wasif <muhammadwasifshah629@gmail.com>
2025-08-19 23:33:58 +0500

    Updated index.html to force HTML renderer

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3190722185"
+    serviceWorkerVersion: "1089330829"
   }
 });
